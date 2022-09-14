75437814091fbeeb8c3e0cb6397ca3efbd5c1a0a
emiruyan <emir-uyn@hotmail.com>
2022-09-14 15:47:14 +0300

    Ball Increase and Ball Text
    As we collect new balls, our number of balls increases and it is written in this text.

--- Hypercasual-Collactable-Balls/Assets/Scripts/BallController.cs
+++ Hypercasual-Collactable-Balls/Assets/Scripts/BallController.cs
@@ -2,9 +2,14 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class BallController : MonoBehaviour
 {
+    [SerializeField] private TMP_Text _ballCountText = null;
+    
+    //Topladığımız topları bu List'te tutacağız.
+    [SerializeField] private List<GameObject> _balls = new List<GameObject>();
     [SerializeField] private float _moveSpeed;
     [SerializeField] private float _horizontalSpeed;
     [SerializeField] private float _horizontalLimit;
@@ -14,6 +19,7 @@ public class BallController : MonoBehaviour
     {
         HorizontalBallMove();
         ForwardBallMove();
+        UpdateBallCountText();
     }
 
     private void HorizontalBallMove()
@@ -42,5 +48,21 @@ public class BallController : MonoBehaviour
     private void ForwardBallMove()
     {
         transform.Translate(Vector3.forward * _moveSpeed * Time.deltaTime);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("StackBall"))//Çarpılan nesnenin tag'ı "StackBall" ise;
+        {
+            other.gameObject.transform.SetParent(transform);//Çarpılan nesneyi child nesne olarak alıyoruz.
+            other.gameObject.GetComponent<SphereCollider>().enabled = false;//Çarpılan nesnenin colliderını devre dışı bıraktık.
+            other.gameObject.transform.localPosition = new Vector3(0f, 0f, _balls[_balls.Count - 1].transform.localPosition.z -1);//Çarpılan nesneyi -1 bir pzisyonunda parent nesnemize ekliyoruz.
+            _balls.Add(other.gameObject);//Çarpılan nesneyi _ball List'ine ekliyoruz. 
+        }
+    }
+
+    private void UpdateBallCountText()
+    {
+        _ballCountText.text = _balls.Count.ToString();//ballCount.text'ini balls List'imizin uzunluğuna atadık.
     }
 }
