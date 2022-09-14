e0e09695b611c40a9d37f9490eeebb630af2bcb5
emiruyan <emir-uyn@hotmail.com>
2022-09-14 20:27:05 +0300

    BallController
    The number of balls increases or decreases as we pass through the gates.

--- Hypercasual-Collactable-Balls/Assets/Scripts/BallController.cs
+++ Hypercasual-Collactable-Balls/Assets/Scripts/BallController.cs
@@ -10,11 +10,13 @@ public class BallController : MonoBehaviour
     
     //Topladığımız topları bu List'te tutacağız.
     [SerializeField] private List<GameObject> _balls = new List<GameObject>();
+    [SerializeField] private GameObject ballPrefab;
     [SerializeField] private float _moveSpeed;
     [SerializeField] private float _horizontalSpeed;
     [SerializeField] private float _horizontalLimit;
     private float _horizontal;
-
+    private int _gateNumber;
+    private int _targetCount;
     private void Update()
     {
         HorizontalBallMove();
@@ -50,6 +52,11 @@ public class BallController : MonoBehaviour
         transform.Translate(Vector3.forward * _moveSpeed * Time.deltaTime);
     }
 
+    private void UpdateBallCountText()
+    {
+        _ballCountText.text = _balls.Count.ToString();//ballCount.text'ini balls List'imizin uzunluğuna atadık.
+    }
+    
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.CompareTag("StackBall"))//Çarpılan nesnenin tag'ı "StackBall" ise;
@@ -59,10 +66,42 @@ public class BallController : MonoBehaviour
             other.gameObject.transform.localPosition = new Vector3(0f, 0f, _balls[_balls.Count - 1].transform.localPosition.z -1);//Çarpılan nesneyi -1 bir pzisyonunda parent nesnemize ekliyoruz.
             _balls.Add(other.gameObject);//Çarpılan nesneyi _ball List'ine ekliyoruz. 
         }
+
+        if (other.gameObject.CompareTag("Gate"))//Çarpılan nesnenin tag'ı "Gate" ise;
+        {
+            _gateNumber = other.gameObject.GetComponent<GateController>().GetGateNumber();//Gate'in değeri ne ise _gateNumber'a atıyoruz
+            _targetCount = _balls.Count + _gateNumber;
+
+            if (_gateNumber > 0)//gateNumber 0'dan büyük ise;(Pozitif)  
+            {
+                IncreaseBallCount(); 
+            }
+           else if (_gateNumber < 0)//gateNumber 0'dan küçük ise;(Negatif)  
+            {
+                DecreaseBallCount();
+            }
+        }
     }
 
-    private void UpdateBallCountText()
+    private void IncreaseBallCount()
     {
-        _ballCountText.text = _balls.Count.ToString();//ballCount.text'ini balls List'imizin uzunluğuna atadık.
+        for (int i = 0; i < _gateNumber; i++)
+        {
+            GameObject _newBall = Instantiate(ballPrefab);
+            _newBall.transform.SetParent(transform);
+            _newBall.GetComponent<SphereCollider>().enabled = false; 
+            _newBall.gameObject.transform.localPosition = new Vector3(0f, 0f, _balls[_balls.Count - 1].transform.localPosition.z -1f);
+            _balls.Add(_newBall);//newBall'ı List'e dahil ettik
+        }
     }
-}
+
+    private void DecreaseBallCount()
+    {
+        for (int i = _balls.Count -1; i  >= _targetCount; i--)
+        {
+            _balls[i].SetActive(false);
+            _balls.RemoveAt(i);
+           
+        }
+    }
+} 
